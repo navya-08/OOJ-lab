7292c6312597cf672657b0010c560fd8eea18972
navya-08 <140879859+navya-08@users.noreply.github.com>
2024-01-17 11:28:27 +0530

    Create Concat.java

---
+++ 16-01-2024/Concat.java
@@ -0,0 +1,10 @@
+ class Concat{
+    public static void main(String[] args) {
+        String s1 = "hello";
+        String s2 = "world";
+        String result = s1.concat(s2);
+        System.out.println("s1: " + s1);
+        System.out.println("s2: " + s2);
+        System.out.println("Concatenated Result: " + result);
+    }
+}
